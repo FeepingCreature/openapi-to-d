03b3d8b5b62ab46da63307e3d910591b9e7379e0
Mathis Beer <Mathis.Beer@funkwerk-itk.com>
2023-04-28 09:21:22 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-NAME="openapi-to-d-$(git describe --always --dirty)"
-docker build --rm -t "$NAME" .
-CONTAINER=$(docker create "$NAME")
-docker cp "$CONTAINER":/tmp/build/openapi-to-d .
-docker rm -v "$CONTAINER"
+docker build -o . .

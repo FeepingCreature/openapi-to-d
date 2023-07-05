5a804887dbbeb5170dcaaffbb88290c767068b1a
Mathis Beer <Mathis.Beer@funkwerk-itk.com>
2023-07-05 14:26:55 +0200

    Add underscore to reserved identifiers in structs.

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,13 @@ RUN apt-get install -y gcc curl xz-utils
 RUN curl https://downloads.dlang.org/releases/2.x/2.099.1/dmd.2.099.1.linux.tar.xz -o dmd.2.099.1.linux.tar.xz && \
   tar xf dmd.2.099.1.linux.tar.xz
 
+RUN sh -c 'mkdir -p /tmp/build'
+
+COPY dub.json dub.selections.json /tmp/build
+
+# fetch dependencies
+RUN sh -c 'cd /tmp/build && PATH=/dmd2/linux/bin64:$PATH dub build || true'
+
 COPY . /tmp/build
 
 RUN sh -c 'cd /tmp/build && PATH=/dmd2/linux/bin64:$PATH dub build'
--- source/app.d
+++ source/app.d
@@ -255,7 +255,7 @@ class Render
         {
             const required = objectType.required.canFind(tableEntry.key);
 
-            members ~= renderMember(tableEntry.key, tableEntry.value, !required, extraTypes);
+            members ~= renderMember(tableEntry.key.fixReservedIdentifiers, tableEntry.value, !required, extraTypes);
             members ~= "\n";
         }
         result ~= extraTypes;
@@ -854,3 +854,39 @@ unittest
     assert("FOO".screamingSnakeToCamelCase == "foo");
     assert("FOO_BAR".screamingSnakeToCamelCase == "fooBar");
 }
+
+private string fixReservedIdentifiers(string name)
+{
+    switch (name)
+    {
+        static foreach (identifier; reservedIdentifiers)
+        {
+        case identifier:
+            return identifier ~ "_";
+        }
+        default:
+            return name;
+    }
+}
+
+private enum reservedIdentifiers = [
+    "abstract", "alias", "align", "asm", "assert", "auto",
+    "body", "bool", "break", "byte",
+    "case", "cast", "catch", "cdouble", "cent", "cfloat", "char", "class", "const", "continue", "creal",
+    "dchar", "debug", "default", "delegate", "delete", "deprecated", "do", "double",
+    "else", "enum", "export", "extern",
+    "false", "final", "finally", "float", "for", "foreach", "foreach_reverse", "function",
+    "goto",
+    "idouble", "if", "ifloat", "immutable", "import", "in", "inout", "int", "interface", "invariant", "ireal", "is",
+    "lazy", "long",
+    "macro", "mixin", "module",
+    "new", "nothrow", "null",
+    "out", "override",
+    "package", "pragma", "private", "protected", "public", "pure",
+    "real", "ref", "return",
+    "scope", "shared", "short", "static", "struct", "super", "switch", "synchronized",
+    "template", "this", "throw", "true", "try", "typeid", "typeof",
+    "ubyte", "ucent", "uint", "ulong", "union", "unittest", "ushort",
+    "version", "void",
+    "wchar", "while", "with",
+];
